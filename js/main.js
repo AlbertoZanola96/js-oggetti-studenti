b0aeaa54e4b5a0ad69e617d319a0f24ccef2313d
alberto <albertozanola1996@gmail.com>
2021-09-03 14:16:58 +0200

    Step 1 creazione oggetto

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 //creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome, età.
+
+let student = {
+    nome: 'alberto',
+    cognome: 'zanola',
+    eta: 25
+}
+
 // stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+
+
+
 // creare un array di oggetti studenti.
 // ciclare su tutti gli studenti e stampare per ogniuno di essi , nome e cognome.
 // dare la possibilità all'utente, attraverso 3 prompt (), di aggiungere un nuovo oggetto studente inserendolo nell'ordine: nome, cognome, età.
