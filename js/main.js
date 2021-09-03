d13ba7b0eefd5073ab1385811e93707c53975b0f
alberto <albertozanola1996@gmail.com>
2021-09-03 14:28:18 +0200

    Step 3 creazione array di oggetti studenti

--- js/main.js
+++ js/main.js
@@ -7,11 +7,36 @@ let student = {
 }
 
 // stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+
 for (let key in student) {
     console.log(key + ': ' + student[key]);
 }
 
 
 // creare un array di oggetti studenti.
+
+let students = [
+    {
+        nome: 'luca',
+        cognome: 'bianchi',
+        eta: 38  
+    },
+    {
+        nome: 'anna',
+        cognome: 'rossi',
+        eta: 18  
+    },
+    {
+        nome: 'stefano',
+        cognome: 'verdi',
+        eta: 55  
+    },
+    {
+        nome: 'giacomo',
+        cognome: 'freddi',
+        eta: 28  
+    }
+];
+
 // ciclare su tutti gli studenti e stampare per ogniuno di essi , nome e cognome.
 // dare la possibilità all'utente, attraverso 3 prompt (), di aggiungere un nuovo oggetto studente inserendolo nell'ordine: nome, cognome, età.
